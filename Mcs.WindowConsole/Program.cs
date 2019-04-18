e2da2537fdbe9c4cfd458564661ac5fe9c9e1857
Luis Gustavo Fernández Batres <elclon_fernanbrans@hotmail.com>
2019-04-18 01:42:39 -0600

    Some modifications for testing purposes

--- Mcs.WindowConsole/Program.cs
+++ Mcs.WindowConsole/Program.cs
@@ -20,13 +20,21 @@ namespace Mcs.WindowConsole {
             AppDomain.CurrentDomain.Load(externalAssembly);
 
             // Now is loading. Creating an instance of class
+            // The object type is "ObjectHandle"
             var hostTransportCommandInstance = Activator.CreateInstance("Mcs.ControlMaster", "Mcs.ControlMaster.HostTransportCommand");
 
             // Getting properties for validate instance creation
+            // Using Unwrap() method we can access to true Type (HostTransportCommand)
             var hostTransportCommandType = hostTransportCommandInstance.Unwrap().GetType();
             var hostTransportCommandProperties = hostTransportCommandInstance.Unwrap().GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
 
-            var temp = 1;
+            // Showing information about type HostTransportCommand
+            Console.WriteLine($"The type from Mcs.ControllerMaster is {hostTransportCommandType}");
+            Console.WriteLine("Showing public properties (they was added for testing purposed):");
+            foreach (PropertyInfo property in hostTransportCommandProperties) {
+                Console.WriteLine(property.Name);
+            }
+            Console.ReadKey();
         }
     }
 }
